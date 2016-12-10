c798e8715a54c550be344a0b56b57d13d15fbbc3
RandomJ <neo3ntino@gmail.com>
2016-12-10 16:43:35 +0100

    Prova commit
    Da non avviare full screen: causa mal di testa

---
+++ UnityProject/Assets/GuardaIlCuboMagico.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GuardaIlCuboMagico : MonoBehaviour {
+
+    GameObject cube;
+    Camera cam;
+
+	// Use this for initialization
+	void Start () {
+        cube = GameObject.Find("Ciao sono un cubo magico roteante");
+        cam = GetComponent<Camera>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        cam.transform.LookAt(cube.transform);
+	}
+}
